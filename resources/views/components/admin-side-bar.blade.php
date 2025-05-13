8044dd56cc7e381b2cf240d57565a58990957154
Nabinacharya <nabinacharya888@gmail.com>
2025-05-13 18:57:02 +0545

    Admin Panel

---
+++ resources/views/components/admin-side-bar.blade.php
@@ -0,0 +1,40 @@
+ <aside id="sidebar-wrapper">
+     <div class="sidebar-brand">
+         <a href="index.html"> <img alt="image" src="/assets/img/logo.png" class="header-logo" />
+             <span class="logo-name">Otika</span>
+         </a>
+     </div>
+     <ul class="sidebar-menu">
+         <li class="menu-header">Main</li>
+
+         <li class="dropdown active">
+             <a href="index.html" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+         </li>
+         <li class="dropdown">
+             <a href="index.html" class="nav-link"><i data-feather="airplay"></i><span>Company</span></a>
+         </li>
+         <li class="dropdown">
+             <a href="index.html" class="nav-link"><i data-feather="list"></i><span>Category</span></a>
+         </li>
+         <li class="dropdown">
+             <a href="index.html" class="nav-link"><i data-feather="book"></i><span>Article</span></a>
+         </li>
+         <li class="dropdown">
+             <a href="index.html" class="nav-link"><i data-feather="airplay"></i><span>Advertise</span></a>
+         </li>
+
+         {{-- <li class="dropdown active">
+                            <a href="#" class="menu-toggle nav-link has-dropdown"><i
+                                    data-feather="command"></i><span>Apps</span></a>
+                            <ul class="dropdown-menu">
+                                <li class="active"><a class="nav-link" href="chat.html">Chat</a></li>
+                                <li><a class="nav-link" href="portfolio.html">Portfolio</a></li>
+                                <li><a class="nav-link" href="blog.html">Blog</a></li>
+                                <li><a class="nav-link" href="calendar.html">Calendar</a></li>
+                            </ul>
+                        </li> --}}
+
+
+
+     </ul>
+ </aside>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    hello nabin
+</x-app-layout>
